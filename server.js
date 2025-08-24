1073831357c7da25c5aea58a5042f086970603f8
Thanakit Chuchoed <tanakit200663@gmail.com>
2025-08-24 11:56:10 +0700

    add docker

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
 
 // Connect MongoDB
 connectDB().then(() => {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   httpServer.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
   );
